e8fb7f63f9640b6d43cf2f174ad11853bc63b70f
Uolevi Holmberg <uolevi.holmberg@gmail.com>
2020-09-13 11:56:12 +0300

    Almost ready

--- js/todo.js
+++ js/todo.js
@@ -4,49 +4,56 @@ window.onload = () => {
   //hae muuttujiin tarvittavat html elementit
   let form = document.getElementById("form");
   let todolist = document.getElementById("todolist");
-  let icons = document.getElementsByClassName('deletebutton');
+  let icons = document.getElementsByClassName("deletebutton");
 
   //form submit = funktio joka luo listan todo-elementit ja lisää sen listaan
   form.onsubmit = () => {
     //luo todo-elementin tarvitsemat elementit
     //lista
+    let content_container = document.createElement("div");
+    content_container.id = "content-container"
     let li = document.createElement("li");
+    li.className = "flexchild";
 
-    //delete nappi
-    let delbtn = document.createElement("div")
-    delbtn.className = "deletebutton";
-    let svg = document.createElement('img');
-    svg.id = "deleteicon"
-    svg.src="icons/del.svg"
-    delbtn.appendChild(svg);
-    let icontext = document.createElement("p");
-    icontext.innerHTML = "Delete";
-    delbtn.appendChild(icontext);
+    //todo sisältö
+    let content = document.createElement("p");
+    content.id = "content";
+    content.append(form.todo.value);
 
     //kommentit span
     let comments = document.createElement("span");
-    //todo input sisältö
-    let content = form.todo.value;
-    //comments input sisältö
-    let commentsText = form.comments.value;
+    comments.id = "comments";
+    comments.append(form.comments.value);
+    
     //timestamp
     let timeStamp = document.createElement("p");
+    timeStamp.id = "timeStamp";
     let time = new Date();
+    timeStamp.append(`Created on: ${time.toLocaleDateString()}`);
+
+    //delete nappi
+    let delbtn = document.createElement("div");
+    delbtn.className = "deletebutton";
+    let svg = document.createElement("img");
+    svg.id = "deleteicon";
+    svg.src = "icons/del.svg";
+    delbtn.appendChild(svg);
+   /*  let icontext = document.createElement("p");
+    icontext.id = "deletetext";
+    icontext.innerHTML = "Delete";
+    delbtn.appendChild(icontext); */
 
     //lisää listaelementtiin sisältö, kommentit, delete nappi ja timestamppi
-    li.append(content);
-    li.appendChild(comments).append(`Comments: ${commentsText}`);
-    comments
-      .appendChild(timeStamp)
-      .append(`Created on: ${time.toLocaleDateString()}`);
-    //timeStamp.appendChild(del);
-    li.appendChild(delbtn);
-    //lisää listaelementti kilkkeineen listaan
-    //todolist.appendChild(li);
-    
+    content_container.appendChild(content);
+    content_container.appendChild(delbtn);
+    content_container.appendChild(comments);
+    content_container.appendChild(timeStamp)
+    li.appendChild(content_container);
+
     //lisätään listaan, uusin tulee aina ylimmäksi
-    todolist.insertAdjacentElement('afterbegin', li);
-   
+    todolist.insertAdjacentElement("afterbegin", li);
+
+    //lisätään eventhandlerin lisäävä funktio luotavaan todo-listaelementtiin
     deleteTodo();
 
     event.preventDefault();
@@ -55,24 +62,21 @@ window.onload = () => {
   //lisätään jokaiseen deleteikoniin eventti joka poistaa isäntäelementin (li)
   let deleteTodo = () => {
     for (let i of icons) {
-       i.addEventListener('click', () => {
-           i.parentElement.remove();
-           event.stopPropagation();
-       })
+      i.addEventListener("click", () => {
+        i.parentElement.parentElement.remove();
+        event.stopPropagation();
+      });
     }
-   }
-   
+  };
 };
 
 //const del = document.querySelector("del");
 
-
 //let comments = document.forms.form.comments;
 
-const clear = document.querySelector(".clear");
+const clear = document.querySelector(".clearbtn");
 
 clear.addEventListener("click", function () {
   todolist.innerHTML = "";
   console.log("clear painettu");
 });
-
